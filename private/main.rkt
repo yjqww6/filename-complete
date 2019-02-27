49bc31cf28a4f18380f52d8261b76c2ab5302320
yjqww6 <343519265@qq.com>
2019-02-27 13:33:05 +0800

    fix for win

--- private/main.rkt
+++ private/main.rkt
@@ -1,10 +1,18 @@
 #lang racket/base
 
-(require racket/string racket/match)
+(require racket/string racket/match racket/format racket/port)
 
 (provide get-completions)
 
 (require srfi/2)
+
+(define (escape str)
+  (define written (~s str))
+  (substring written 1 (- (string-length written) 1)))
+
+(define (unescape str)
+  (with-handlers ([exn:fail:read? (λ (_) #f)])
+    (read (open-input-string (string-append "\"" str "\"")))))
 
 (define (parse-path str)
   (define-values (base sub dir?) (split-path str))
@@ -15,21 +23,25 @@
     [else (values base sub)]))
 
 (define (get-completions str)
-  (cond
-    [(string=? str "") (map path->string (directory-list))]
-    [else
-     (define-values (b s) (parse-path str))
-     (cond
-       [(not s)
-        (if (directory-exists? str)
-            (map (λ (f)
-                   (string-append
-                    (path->string (path->directory-path b))
-                    (path->string f)))
-                 (directory-list str))
-            '())]
-       [else
-        (map (λ (f)
-               (path->string (build-path b f)))
-             (filter (λ (f) (string-prefix? (path->string f) (path->string s)))
-                     (directory-list b)))])]))
+  (and-let*
+   ([str (unescape str)])
+   (map
+    escape
+    (cond
+      [(string=? str "") (map path->string (directory-list))]
+      [else
+       (define-values (b s) (parse-path str))
+       (cond
+         [(not s)
+          (if (directory-exists? str)
+              (map (λ (f)
+                     (string-append
+                      (path->string (path->directory-path b))
+                      (path->string f)))
+                   (directory-list str))
+              '())]
+         [else
+          (map (λ (f)
+                 (path->string (build-path b f)))
+               (filter (λ (f) (string-prefix? (path->string f) (path->string s)))
+                       (directory-list b)))])]))))
